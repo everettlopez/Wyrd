f1d933451dd0d80cc4b40159c092a5302c83aa6f
Everett Lopez <everettlopezsr@gmail.com>
2025-05-12 11:21:10 -0700

    Set the dimensions of the GamePage window

--- Wyrd/App.xaml.cs
+++ Wyrd/App.xaml.cs
@@ -5,9 +5,22 @@
         public App()
         {
             InitializeComponent();
-
+            
             // Wrap the AppShell in a NavigationPage to support navigation features
             MainPage = new NavigationPage(new AppShell());
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 450;
+            const int newHeight = 650;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
         }
     }
 }
--- Wyrd/GamePage.xaml.cs
+++ Wyrd/GamePage.xaml.cs
@@ -24,6 +24,7 @@ public partial class GamePage : ContentPage
     public GamePage()
     {
         InitializeComponent();
+
         _ = InitializeGameAsync(); // Fire-and-forget async call
     }
 
